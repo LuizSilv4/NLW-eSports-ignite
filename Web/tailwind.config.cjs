9119c5925eef40cbda179e04d69399ef0bd6427f
LuizSilv4 <107224710+LuizSilv4@users.noreply.github.com>
2022-09-22 13:15:42 -0300

    Add files via upload

---
+++ Web/tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+  './index.html'],
+  theme: {
+    fontFamily: {
+    sans: ['Inter', 'Sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy:"url('/Fundo.png')",
+        'gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
+  },
+  plugins: [],
+}
